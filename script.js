251bdf380cef8408dd8f78bec320a63292a55730
yoshi <yositha.k@gmail.com>
2024-01-04 23:36:56 +0530

    first

---
+++ script.js
@@ -0,0 +1,34 @@
+let display = document.getElementById('display');
+let expression = '';
+
+function appendToDisplay(value) {
+  expression += value;
+  display.value = expression;
+}
+
+function clearDisplay() {
+  expression = expression.slice(0,-1);
+  display.value = expression;
+
+}
+
+function reset() {
+  expression = '';
+  display.value = '0';
+  }
+
+function calculate() {
+  try {
+    let result = eval(expression);
+    if (Number.isFinite(result)) {
+      display.value = result.toFixed(3); 
+      expression = '';
+    } else {
+      display.value = 'Error';
+      expression = '';
+    }
+  } catch (error) {
+    display.value = 'Error';
+    expression = '';
+  }
+}
